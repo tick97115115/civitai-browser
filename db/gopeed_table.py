aa5e2d539657340092de31455e99e4861b4b18fd
Pyon~ Pyon~~ <32931796+tick97115115@users.noreply.github.com>
2025-03-20 22:21:28 +0800

    update

---
+++ db/gopeed_table.py
@@ -0,0 +1,16 @@
+from sqlmodel import SQLModel, Field, Relationship
+from pydantic import StrictInt
+
+class ModelVersionGopeedTask(SQLModel, table=True):
+    version_id: StrictInt = Field(primary_key=True)
+    task_id: str
+    file_name: str
+    path: str
+    image_tasks: list["ModelVersionImageGopeedTask"] = Relationship(back_populates="model_version_task")
+
+class ModelVersionImageGopeedTask(SQLModel, table=True):
+    version_id: StrictInt = Field(foreign_key="model_version_gopeed_task.version_id", index=True)
+    task_id: str
+    file_name: str
+    path: str
+    model_version_task: ModelVersionGopeedTask = Relationship(back_populates="image_tasks")
